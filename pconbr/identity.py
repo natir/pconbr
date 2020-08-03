367169c5be5d306b54c719e4c308a3c95a601398
Pierre Marijon <pierre.marijon@hhu.de>
2020-08-03 12:30:43 +0200

    Update identity script to new br correction

--- pconbr/identity.py
+++ pconbr/identity.py
@@ -9,7 +9,8 @@ from collections import defaultdict
 import pandas
 
 def get_parameter(filename):
-    match = re.match(".+\.k(?P<k>\d+)(\.n(?P<n>\d+))?(\.a(?P<a>\d+))?\.s(?P<s>\d+).stats", filename)
+    simulated_reads_96.k11.a18.one-greedy-gap_size.stats
+    match = re.match(".+\.k(?P<k>\d+)(\.a(?P<a>\d+))?\.(?P<method>.+)\.stats", filename)
 
     ret = {k: int(v) for k, v in match.groupdict().items() if v is not None}
     return ret
@@ -30,21 +31,21 @@ def genomic_kmer(filename):
             if entry.is_file() and entry.name.startswith(filename) and entry.name.endswith(".stats"):
                 param = get_parameter(entry.name)
                 k = param['k']
-                s = param['s']
+                m = param['method']
                 
                 error_rate = get_error_rate(entry.path)
                 if error_rate is None:
-                    data[k][s] = -1
+                    data[k][m] = -1
                 else:
-                    data[k][s] = float(error_rate) - raw_error_rate
+                    data[k][m] = float(error_rate) - raw_error_rate
 
     all_k = sorted(list({k for k in data.keys()}))
-    all_s = sorted(list({s for k in data.keys() for s in data[k].keys()}))
+    all_m = sorted(list({s for k in data.keys() for m in data[k].keys()}))
 
     df = pandas.DataFrame(index=[k for k in all_k])
     df.index.name = "k"
     
-    for s in all_s:
+    for m in all_m:
         tmp_list = list()
         for k in df.index:
             if k not in data:
@@ -54,7 +55,7 @@ def genomic_kmer(filename):
             else:
                 tmp_list.append(data[k][s] * 100)
         
-        df["s{}".format(s)] = tmp_list
+        df["{}".format(m)] = tmp_list
 
     return df
 
@@ -69,22 +70,22 @@ def read_kmer(filename):
                 param = get_parameter(entry.name)
                 k = param["k"]
                 a = param["a"]
-                s = param["s"]
+                m = param["m"]
                 
                 error_rate = get_error_rate(entry.path)
                 if error_rate is None:
-                    data[k][a][s] = -1
+                    data[k][a][m] = -1
                 else:
-                    data[k][a][s] = float(error_rate) - raw_error_rate
+                    data[k][a][m] = float(error_rate) - raw_error_rate
                     
     all_k = sorted(list({k for k in data.keys()}))
     all_a = sorted(list({a for k in data.keys() for a in data[k].keys()}))                
-    all_s = sorted(list({s for k in data.keys() for a in data[k].keys() for s in data[k][a].keys()}))
+    all_m = sorted(list({s for k in data.keys() for a in data[k].keys() for m in data[k][a].keys()}))
 
     index = pandas.MultiIndex.from_tuples([(a, k) for a in all_a for k in all_k], names=("a", "k"))
     df = pandas.DataFrame(index=index)
 
-    for s in all_s:
+    for m in all_m:
         tmp_list = list()
         for a, k in df.index:
             if k not in data:
@@ -94,8 +95,8 @@ def read_kmer(filename):
             elif s not in data[k][a]:
                 tmp_list.append(None)
             else:
-                tmp_list.append(data[k][a][s] * 100)
+                tmp_list.append(data[k][a][m] * 100)
 
-        df["s{}".format(s)] = tmp_list
+        df["{}".format(m)] = tmp_list
                 
     return df
