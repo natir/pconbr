d91f0b0e59f6e158cd234f460f3fac3a1fc0b36b
Pierre Marijon <pierre.marijon@hhu.de>
2020-09-23 15:54:28 +0200

    Add dataset bacteria5

--- cocktail/kmer_spectrum.py
+++ cocktail/kmer_spectrum.py
@@ -2,15 +2,18 @@ import csv
 import pandas
 
 def get_kmer_spectrum(dataset, kmer_size):
-    ref_count_kmer = csv.reader(open("count/{dataset}/pcon/reference.k{kmer_size}.csv"))
-    read_count_kmer = csv.reader(open("data/{dataset}/pcon/reads.k{kmer_size}.csv"))
+    ref_count_kmer = csv.reader(open(f"count/{dataset}/pcon/reference.k{kmer_size}.csv"))
+    read_count_kmer = csv.reader(open(f"count/{dataset}/pcon/reads.k{kmer_size}.csv"))
 
     all_kmer = [0 for _ in range(0, 256)]
     true_kmer = [0 for _ in range(0, 256)]
     false_kmer = [0 for _ in range(0, 256)]
 
+    print("begin set of true kmer")
     ref_kmer = {kmer for (kmer, count) in ref_count_kmer}
+    print("end set of true kmer")
 
+    print("begin compute histograme")
     for (kmer, count) in read_count_kmer:
         count = int(count)
 
@@ -19,6 +22,13 @@ def get_kmer_spectrum(dataset, kmer_size):
             true_kmer[count] += 1
         else:
             false_kmer[count] += 1
+    print("end compute histograme")
+    
+    df = pandas.DataFrame(data={"all": all_kmer, "true": true_kmer, "false": false_kmer}, index=range(0, 256))
+    return df
 
-    pandas.DataFrame(data=(all_kmer, true_kmer, false_kmer), columns=["all", "true", "false"])
-        
+def figure(df, column):
+    return altair.Chart(df).mark_bar().encode(
+        x=altair.X(column, bin=altair.Bin(maxbins=255), scale=altair.Scale(type='log', base=10)),
+        y=altair.Y('count()'),
+    )
