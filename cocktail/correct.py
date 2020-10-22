c4f554bd1d9a498d7c449a445132dfc1bbc1d519
Pierre Marijon <pierre.marijon@hhu.de>
2020-10-22 17:05:10 +0200

    Improve graph generation for correct data

--- cocktail/correct.py
+++ cocktail/correct.py
@@ -4,22 +4,21 @@ import math
 import pandas
 import altair
 
-from .utils import get_bench_data
+from . import utils
 
-
-def dataframe_pr():
+def dataframe_elector():
     data = list()
 
-    for dataset in ["bacteria", "yeast", "metagenome", "bacteria5", "bacteria7"]:
+    for dataset in utils.get_data_set("data"):
         for kmer_size in range(13, 21, 2):
-            (precision, recall) = get_data_pr("br", dataset, kmer_size)
+            (precision, recall) = get_data_elector("br", dataset, kmer_size)
             
             if precision is not None and recall is not None:
                 data.append((f"br_k{kmer_size}", dataset, precision, recall))
     
     for corrector in ["canu", "consent", "necat"]:
         for dataset in ["bacteria", "yeast", "metagenome"]:
-                (precision, recall) = get_data_pr(corrector, dataset, None)
+                (precision, recall) = get_data_elector(corrector, dataset, None)
                 
                 if precision is not None and recall is not None:
                     data.append((corrector, dataset, precision, recall))
@@ -27,7 +26,7 @@ def dataframe_pr():
     return  pandas.DataFrame(data, columns=['corrector', 'dataset', 'precision', 'recall'])
 
     
-def get_data_pr(corrector, dataset, kmer_size=None):
+def get_data_elector(corrector, dataset, kmer_size=None):
     if kmer_size is not None:
         path = f"correct/{dataset}/elector/{corrector}/reads.k{kmer_size}/log"
     else:
@@ -51,22 +50,72 @@ def get_data_pr(corrector, dataset, kmer_size=None):
 
 def dataframe_bench():
     data = list()
-    
-    for dataset in ["bacteria", "yeast", "metagenome", "bacteria5", "bacteria7"]:
+
+    d2size = {d: size / 1_000_000 for (d, size) in map(lambda x: (x, utils.get_file_size(x)), utils.get_data_set("data")) if size is not None}
+        
+    for dataset in d2size.keys():
         for kmer_size in range(13, 21, 2):
             (time, memory) = get_data_bench("br", dataset, f".k{kmer_size}")
-            if time is not None:
-                data.append((dataset, f"br_k{kmer_size}", time, memory))
 
+            if time is not None:
+                data.append((dataset, f"br_k{kmer_size}", time, memory, d2size[dataset]))
+                
         for corrector in ["canu", "consent", "necat"]:
             (time, memory) = get_data_bench(corrector, dataset, "")
-            if time is not None:
-                data.append((dataset, corrector, time, memory))
 
-    return pandas.DataFrame(data, columns=['dataset', 'corrector', 'time', 'memory'])
+            if time is not None:
+                data.append((dataset, corrector, time, memory, d2size[dataset]))
+                
+    return pandas.DataFrame(data, columns=['dataset', 'corrector', 'time', 'memory', 'size'])
 
 
 def get_data_bench(corrector, dataset, params):
     path = f"correct/bench/{corrector}/{dataset}_reads{params}.tsv"
         
-    return get_bench_data(path)
+    return utils.get_bench_data(path)
+
+
+def dataframe_stats():
+    data = list()
+    
+    d2error = {d: e for (d, e) in map(lambda x: (x, get_error_rate_raw(x)), utils.get_data_set("data")) if e is not None}
+       
+    for dataset in d2error.keys():
+        for kmer_size in range(13, 21, 2):
+            error_rate = get_error_rate_correct("br", dataset, f".k{kmer_size}")
+
+            if error_rate is not None:
+                data.append((dataset, f"br_k{kmer_size}", error_rate, d2error[dataset]))
+                
+        for corrector in ["canu", "consent", "necat"]:
+            error_rate = get_error_rate_correct(corrector, dataset, "")
+
+            if error_rate is not None:
+                data.append((dataset, corrector, error_rate, d2error[dataset]))
+                
+    return pandas.DataFrame(data, columns=['dataset', 'corrector', 'corrected', 'raw'])
+
+
+def get_error_rate_correct(corrector, dataset, params):
+    path = f"correct/{dataset}/{corrector}/reads{params}.stats"
+
+    return get_error_rate(path)
+
+
+def get_error_rate_raw(dataset):
+    path = f"data/{dataset}/reads.stats"
+
+    return get_error_rate(path)
+
+
+def get_error_rate(path):
+    if not os.path.isfile(path):
+        return None
+    
+    with open(path) as fh:
+        for line in fh:
+            if line.startswith("SN\terror rate:"):
+                return float(line.split("\t")[2])
+
+    return None
+
--- cocktail/utils.py
+++ cocktail/utils.py
@@ -17,18 +17,21 @@ def get_bench_data(path):
 def get_file_size(dataset):
     path = f"data/{dataset}/reads.fasta"
 
+    if not os.path.isfile(path):
+        return None
+    
     return os.path.getsize(path)
 
     
-def group_scatter(df, x, y, color=None, shape=None, title=None):
+def group_scatter(df, x, y, color=None, shape=None, title="", xtitle="", ytitle=""):
 
     xrange = (math.floor(df[x].min() * 100) / 100, math.ceil(df[x].max() * 100) / 100)
     yrange = (math.floor(df[y].min() * 100) / 100, math.ceil(df[y].max() * 100) / 100)
 
-    fig =  altair.Chart(df, title=title).mark_point().encode(
-            x=altair.X(x, scale=altair.Scale(domain=xrange)),
-            y=altair.Y(y, scale=altair.Scale(domain=yrange))
-        )
+    fig = altair.Chart(df, title=title).mark_point().encode(
+        x=altair.X(x, scale=altair.Scale(domain=xrange), title=xtitle),
+        y=altair.Y(y, scale=altair.Scale(domain=yrange), title=ytitle)
+    )
 
     if color:
         fig = fig.encode(color=color)
@@ -39,15 +42,15 @@ def group_scatter(df, x, y, color=None, shape=None, title=None):
     return fig
 
 
-def group_line(df, x, y, color=None, shape=None, column=None):
+def group_line(df, x, y, color=None, shape=None, column=None, title="", xtitle="", ytitle="", point=False):
 
     xrange = (math.floor(df[x].min() * 100) / 100, math.ceil(df[x].max() * 100) / 100)
     yrange = (math.floor(df[y].min() * 100) / 100, math.ceil(df[y].max() * 100) / 100)
 
-    fig =  altair.Chart(df).mark_line().encode(
-            x=altair.X(x, scale=altair.Scale(domain=xrange)),
-            y=altair.Y(y, scale=altair.Scale(domain=yrange))
-        )
+    fig = altair.Chart(df, title=title).mark_line(point=point).encode(
+        x=altair.X(x, scale=altair.Scale(domain=xrange), title=xtitle),
+        y=altair.Y(y, scale=altair.Scale(domain=yrange), title=ytitle)
+    )
 
     if color:
         fig = fig.encode(color=color)
@@ -59,3 +62,8 @@ def group_line(df, x, y, color=None, shape=None, column=None):
         fig = fig.encode(column=column)
     
     return fig
+
+def get_data_set(step):
+    for entry in os.scandir(step):
+        if entry.is_dir():
+            yield entry.name
