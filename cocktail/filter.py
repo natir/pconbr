281cc0091c69502c7bbb34f96a0080a0c3294b84
Pierre Marijon <pierre.marijon@hhu.de>
2020-10-23 16:15:10 +0200

    Move filter code from notebook to lib

--- cocktail/filter.py
+++ cocktail/filter.py
@@ -5,9 +5,30 @@ import altair
 
 from . import utils
 
-def figure(df, color=None):
-    domain_len = (df["length"].min(), df["length"].max())
-    domain_ide = (df["identity"].min(), 100)
+def identity_data(dataset, filter, params):
+    data = list()
+
+    path = f"filter/{dataset}/identity/{filter}/reads.{params}.tsv"      
+    return get_id_data(path, dataset, filter, params)
+ 
+
+def get_id_data(path, dataset, filter, params):
+    data = list()
+    
+    with open(path) as fh:
+        reader = csv.DictReader(fh, delimiter='\t')
+        for row in reader:
+            data.append((dataset, filter, params, row["name"], int(row["length"]), float(row["identity"])))
+
+    return pandas.DataFrame(data, columns=['dataset', 'tool', 'params', 'name', 'length', 'identity'])
+
+
+def figure_len_identity(df, len_filter=0.998, color=None):
+    max = df.quantile(len_filter).length
+    df = df.loc[df.length<max]
+    
+    domain_len = (df.length.min(), df.length.max())
+    domain_ide = (df.identity.min(), 100)
     
     readlength = altair.Chart(df).mark_bar().encode(
         x=altair.X("length:Q", bin=altair.Bin(maxbins=200), scale=altair.Scale(domain=domain_len), axis=None),
@@ -33,42 +54,22 @@ def figure(df, color=None):
         height=500
     )
 
-    if filter:
+    if color:
         scatter = scatter.encode(color=color)
         readlength = readlength.encode(color=color)
         quality = quality.encode(color=color)
 
     return readlength & (scatter | quality) 
 
+def figure_filtred(dataframe, filter, params, len_filter=0.998):
+    df_raw = get_id_data(f"data/{dataframe}/reads.len_id.tsv", "yeast", "raw", "")
 
-def dataframe():
-    data = list()
+    df_filter = identity_data(dataframe, filter, params)
+    filter_name = set(df_filter.name)
 
-    for dataset in ["bacteria", "bacteria5", "bacteria7", "yeast", "metagenome"]:
-        for kmer_size in range(13, 21, 2):
-            for ratio in range(70, 100, 5):
-                path = f"filter/{dataset}/identity/kmrf/reads.k{kmer_size}.r{ratio}.tsv"
-                if os.path.isfile(path):
-                    data += get_data(path, dataset, f"kmrf_k{kmer_size}_r{ratio}")
-                    
-    for dataset in ["bacteria", "yeast", "metagenome"]:
-        for quality in range(90, 100, 1):
-            path = f"filter/{dataset}/identity/filtlong/reads.q{quality}.tsv"
-            if os.path.isfile(path):
-                data += get_data(path, dataset, f"filtlong_q{quality}")
-                            
-    return pandas.DataFrame(data, columns=['dataset', 'tool', 'name', 'length', 'identity'])
+    df_raw["filtered"] = df_raw.name.apply(lambda x: x not in filter_name)
 
-
-def get_data(path, dataset, filter):
-    data = list()
-    
-    with open(path) as fh:
-        reader = csv.DictReader(fh, delimiter='\t')
-        for row in reader:
-            data.append((dataset, filter, row["name"], int(row["length"]), float(row["identity"])))
-
-    return data
+    return figure_len_identity(df_raw, len_filter=len_filter, color="filtered")
 
 
 def dataframe_bench():
